1c3f12c75eb4f77e43ca4233b5833954be65e1cc
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2025-04-30 01:16:39 +0600

    code update

--- Program.cs
+++ Program.cs
@@ -11,8 +11,8 @@ var app = builder.Build();
 
 if(app.Environment.IsDevelopment())
 {
-  app.UseSwagger();
-  app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -24,49 +24,71 @@ List<Category> categories = new List<Category>();
 app.MapGet( "/", () =>"My First Project");
 //Get Mathod.............................! 
 app.MapGet("/api/categories", ( [FromQuery]string searchValue="")=>{
-  Console.WriteLine($"{searchValue}");
-  if(!String.IsNullOrEmpty(searchValue)){
-   var searchValueCategory = categories.Where(c =>c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
-      return Results.Ok(searchValueCategory);
-  }
-  
-  return Results.Ok(categories); // 200
+Console.WriteLine($"{searchValue}");
+if(!String.IsNullOrEmpty(searchValue)){
+  var searchValueCategory = categories.Where(c =>c.Name.Contains(searchValue, StringComparison.OrdinalIgnoreCase)).ToList();
+    return Results.Ok(searchValueCategory);
+}
+
+return Results.Ok(categories); // 200
 });
 
 //Post Method.........................!
 app.MapPost("/api/categories", ([FromBody] Category CategoryInData) =>{
-  // Console.WriteLine($"{CategoryInData}");
- var newCategory = new Category(){
-     CategoryId = Guid.NewGuid(),
-     Name = CategoryInData.Name,
-     Description = CategoryInData.Description,
-     CreateTime = DateTime.UtcNow,
- };
-  categories.Add(newCategory);
-  return Results.Created($"/api/categories{newCategory.CategoryId}",newCategory);
- 
+
+if(string.IsNullOrEmpty(CategoryInData.Name)){
+  return Results.BadRequest("category name is required and can not by empty");
+}
+if(CategoryInData.Name.Length > 5){
+  return Results.BadRequest("Category name is must be atleast 5 characters...");
+}
+if(char.IsUpper(CategoryInData.Name[0]))
+{
+  return Results.BadRequest("The first character is in upper case...");
+}
+var newCategory = new Category(){
+    CategoryId = Guid.NewGuid(),
+    Name = CategoryInData.Name,
+    Description = CategoryInData.Description,
+    CreateTime = DateTime.UtcNow,
+};
+categories.Add(newCategory);
+return Results.Created($"/api/categories{newCategory.CategoryId}",newCategory);
+
 });
 
 //Put Mathod...........................! 
-app.MapPut("/api/categories/{categoryId}",( Guid categoryId ,[FromBody] Category CategoryInData)=>{
-  var FoundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
+app.MapPut("/api/categories/{categoryId:guid}",( Guid categoryId ,[FromBody] Category CategoryInData)=>{
+if(CategoryInData == null){
+  return Results.BadRequest("Category with this id does not missing");
+}
+var FoundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
 
-  if(FoundCategory == null){
-    return Results.NotFound("This Category in Update is not found.......");
+if(FoundCategory == null){
+  return Results.NotFound("This Category in Update is not found.......");
+}
+
+if(!string.IsNullOrEmpty(CategoryInData.Name)){
+  if(CategoryInData.Name.Length >= 2){
+    FoundCategory.Name = CategoryInData.Name;
+  } else{
+    return Results.BadRequest("category Name is Must be atleast 2 characters long......");
   }
-  FoundCategory.Name = CategoryInData.Name;
+}
+if(!string.IsNullOrWhiteSpace(CategoryInData.Description)){
   FoundCategory.Description = CategoryInData.Description;
-  return Results.NoContent(); //204
+}
+return Results.NoContent(); //204
 });
 
 //Delete Mathod...........................! 
-app.MapDelete("/api/categories/{categoryId}", (Guid categoryId)=>{
-  var FoundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
-  if(FoundCategory == null){
-    return  Results.NotFound("Category with this is does Not Exist..");
-  }
-   categories.Remove(FoundCategory);
-  return Results.NoContent();//204
+app.MapDelete("/api/categories/{categoryId:guid}", (Guid categoryId)=>{
+var FoundCategory = categories.FirstOrDefault(category => category.CategoryId == categoryId);
+if(FoundCategory == null){
+  return  Results.NotFound("Category with this is does Not Exist..");
+}
+  categories.Remove(FoundCategory);
+return Results.NoContent();//204
 });
 
 
@@ -77,9 +99,9 @@ app.Run();
 
 public record Category
 {
-  public Guid CategoryId{get; set;}
-  public String Name{get; set;}
-  public String? Description{get; set;}
-  public DateTime CreateTime{get; set;}
+public Guid CategoryId{get; set;}
+public String Name{get; set;}
+public String Description{get; set;} = string.Empty;
+public DateTime CreateTime{get; set;}
 
 }
