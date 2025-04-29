dbe4dd3ae09107b5989bb00645a30e73b298a9ed
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2025-04-29 08:29:07 +0600

    model

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,17 +28,38 @@ app.MapGet("/api/categories", ()=>{
 });
 
 //Post Method.........................!
-app.MapPost("/api/categories", () =>{
- return Results.Created();
+app.MapPost("/api/categories", ([FromBody] Category CategoryInData) =>{
+  // Console.WriteLine($"{CategoryInData}");
+ var newCategory = new Category(){
+     CategoryId = Guid.NewGuid(),
+     Name = CategoryInData.Name,
+     Description = CategoryInData.Description,
+     CreateTime = DateTime.UtcNow,
+ };
+  categories.Add(newCategory);
+  return Results.Created($"/api/categories{newCategory.CategoryId}",newCategory);
+ 
 });
 
 //Put Mathod...........................! 
 app.MapPut("/api/categories", ()=>{
+  var FoundCategory = categories.FirstOrDefault(category => category.CategoryId ==Guid.Parse("ed7b8745-bb62-4e59-928f-6ed031d2ff1e"));
+
+  if(FoundCategory == null){
+    return Results.NotFound("This Category in Update is not found.......");
+  }
+  FoundCategory.Name = "smart category";
+  FoundCategory.Description = "My Smary Phone";
   return Results.NoContent(); //204
 });
 
 //Delete Mathod...........................! 
 app.MapDelete("/api/categories", ()=>{
+  var FoundCategory = categories.FirstOrDefault(category => category.CategoryId == Guid.Parse("ed7b8745-bb62-4e59-928f-6ed031d2ff1e"));
+  if(FoundCategory == null){
+    return  Results.NotFound("Category with this is does Not Exist..");
+  }
+   categories.Remove(FoundCategory);
   return Results.NoContent();//204
 });
 
