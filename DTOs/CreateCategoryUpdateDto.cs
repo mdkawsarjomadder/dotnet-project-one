56ec56a891aa09500599d173c53f6ebede6d0517
Md Kawar <60819454+mdkawsarjomadder@users.noreply.github.com>
2025-05-04 23:25:03 +0600

    validation add

--- DTOs/CreateCategoryUpdateDto.cs
+++ DTOs/CreateCategoryUpdateDto.cs
@@ -10,7 +10,7 @@ namespace dotnet_project_one.DTOs
     {
         [StringLength(100, MinimumLength = 4, ErrorMessage = "Category Name is Must Be Between 4 and 100 characters...")]
         public String Name{get; set;} = string.Empty;
-        [StringLength(500, ErrorMessage ="Category description Cannot is exceed 500 characters...!")]
+        [StringLength(500,MinimumLength = 10, ErrorMessage ="Category Description is Must Be Between 10 and 500 characters...")]
         public String Description{get; set;} = string.Empty;
     }
 }
